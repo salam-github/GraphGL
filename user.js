63b4cd8f78ebecf1e23b8fcefda1baceb900acc4
Your Name <you@example.com>
2023-05-03 15:45:11 +0300

    users.js modify

--- user.js
+++ user.js
@@ -77,3 +77,155 @@ let getData = async (username) => {
         lastXP = task.xp;
     });
 };
+
+const xpOverTime = (_) => {
+    let charts = document.getElementById("charts");
+    let xpOverTime = document.createElement("div");
+    charts.setAttribute("id", "chart1");
+    charts.appendChild(xpOverTime);
+    charts.appendChild(document.createElement("br"));
+
+    // set dimensions adn margins of graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+        //append the svg object to the body of the page
+        var svg = d3
+        .select("#chart1")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr(transform = "translate(" + margin.left + "," + margin.top + ")");
+
+        // Add X axis --> it is a date format
+        var x = d3
+        .scaleTime()
+        .domain(
+            d3.extent(userData.xpandDate, function (d) {
+                return d.date;
+            })
+        )
+        .range([0, width]);
+
+        svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3
+        .scaleLinear()
+        .domain([
+            0,
+            d3.max(userData.xpandDate, function (d) {
+                return +d.xp;
+            }),
+        ])
+        .range([height, 0]);
+
+        svg
+        .append("g")
+        .call(d3.axisLeft(y));
+        append("text")
+        .atrr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("XP");
+
+        // Add the line
+        svg
+        .append("path")
+        .datum(userData.xpandDate)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr(
+            "d",
+            d3
+            .line()
+            .x(function (d) {
+                return x(d.date);
+            })
+            .y(function (d) {
+                return y(d.xp);
+            })
+        );
+};
+
+const xpByProject = (_) => {
+    let charts = document.getElementById("charts");
+    let chart = document.createElement("div");
+    chart.setAttribute("id", "chart2");
+    charts.appendChild(chart);
+
+    // set dimensions  and margins of the page
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3
+    .select("#chart2")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr(transform = "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis --> 
+    var x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(
+        userData.xpByProject.map(function (d) {
+            return d.project;
+        })
+    )
+    .padding(0.2);
+    svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+    // Add Y axis
+    var y = d3
+    .scaleLinear()
+    .domain([0, Math.max(...userData.xpByProject.map((o) => o.xp)) + 5000])
+    .range([height, 0]);
+
+    svg
+    .append("g")
+    .call(d3.axisLeft(y));
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("XP");
+
+    // Bars
+    svg
+    .selectAll("mybar")
+    .data(userData.xpByProject)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+        return x(d.project);
+    })
+    .attr("y", function (d) {
+        return y(d.xp);
+    })
+    .attr("width", x.bandwidth())
+    .attr("height", function (d) {
+        return height - y(d.xp);
+    })
+    .attr("fill", "#69b3a2");
+};
