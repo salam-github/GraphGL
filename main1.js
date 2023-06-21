1b5045d1e8b6026fd68e3d9324fc90fda1870205
salam <salamfoon@gmail.com>
2023-06-21 16:44:35 +0300

    mostly working nowwww

--- main1.js
+++ main1.js
@@ -113,6 +113,7 @@ export function loadUserData() {
       //drawData(user.login, user.firstName + " " + user.lastName, user.totalXp);
 	  drawPieChart(data.audits);
 	  drawHorizontalBarChart(data.skills);
+	  drawLineGraph(data.xp);
 	  
 
 
@@ -358,158 +359,82 @@ function drawHorizontalBarChart(skills) {
 
 
 
+  function drawLineGraph(xpData) {
+	// Extracting the relevant data for the line graph
+	const data = xpData.map((xp, index) => ({
+	  date: new Date(xp.createdAt),
+	  amount: xp.amount
+	}));
+  
+	// Setting up chart dimensions
+	const margin = { top: 20, right: 30, bottom: 30, left: 50 };
+	const width = 600 - margin.left - margin.right;
+	const height = 400 - margin.top - margin.bottom;
+  
+	// Creating SVG element
+	const svg = d3.select("#chart-container")
+	  .append("svg")
+	  .attr("width", width + margin.left + margin.right)
+	  .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+	// Creating scales
+	const xScale = d3.scaleTime()
+	  .domain(d3.extent(data, d => d.date))
+	  .range([0, width]);
+  
+	const yScale = d3.scaleLinear()
+	  .domain([0, d3.max(data, d => d.amount)])
+	  .range([height, 0]);
+  
+	// Creating line generator
+	const line = d3.line()
+	  .x(d => xScale(d.date))
+	  .y(d => yScale(d.amount));
+  
+	// Adding the line
+	svg.append("path")
+	  .datum(data)
+	  .attr("fill", "none")
+	  .attr("stroke", "steelblue")
+	  .attr("stroke-width", 1.5)
+	  .attr("d", line);
+  
+	// Adding the x-axis
+	svg.append("g")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(d3.axisBottom(xScale));
+  
+	// Adding the y-axis
+	svg.append("g")
+	  .call(d3.axisLeft(yScale));
+  
+	// Adding labels
+	svg.append("text")
+	  .attr("text-anchor", "middle")
+	  .attr("transform", "translate(" + (width / 2) + "," + (height + margin.top + 20) + ")")
+	  .text("Date");
+  
+	svg.append("text")
+	  .attr("text-anchor", "middle")
+	  .attr("transform", "translate(" + (-margin.left + 20) + "," + (height / 2) + ")rotate(-90)")
+	  .text("XP Amount");
+  }
+  
+  // Assuming the variable 'audits' contains the data
+//   const xpData = audits.filter(audit => audit.path.startsWith("/gritlab/school-curriculum/"));
+//   drawLineGraph(xpData);
+  
 
 
 
+function drawLevel(Level){
 
-
-const xpOverTimeChart = (_) => {
-	let charts = document.getElementById("graphs");
-	let chart = document.createElement("div");
-	chart.setAttribute("id", "chart1");
-	charts.appendChild(chart);
-	charts.appendChild(document.createElement("br"));
-
-	// set the dimensions and margins of the graph
-	var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-		width = 800 - margin.left - margin.right,
-		height = 600 - margin.top - margin.bottom;
-
-	// append the svg object to the body of the page
-	var svg = d3
-		.select("#chart1")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// Add X axis --> it is a date format
-	var x = d3
-		.scaleTime()
-		.domain(
-			d3.extent(userData.xpAndDate, function (d) {
-				return d.date;
-			})
-		)
-		.range([0, width]);
-	svg
-		.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
-
-	// Add Y axis
-	var y = d3
-		.scaleLinear()
-		.domain([
-			0,
-			d3.max(userData.xpAndDate, function (d) {
-				return +d.xp;
-			}),
-		])
-		.range([height, 0]);
-	svg
-		.append("g")
-		.call(d3.axisLeft(y))
-		.append("text")
-		.attr("fill", "#000")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("TOTAL XP");
-
-	// Add the line
-	svg
-		.append("path")
-		.datum(userData.xpAndDate)
-		.attr("fill", "none")
-		.attr("stroke", "#0b7dda")
-		.attr("stroke-width", 5)
-		.attr(
-			"d",
-			d3
-				.line()
-				.x(function (d) {
-					return x(d.date);
-				})
-				.y(function (d) {
-					return y(d.xp);
-				})
-		);
-};
+}
 
 const xpByProjectChart = (_) => {
-	let charts = document.getElementById("graphs");
-	let chart = document.createElement("div");
-	chart.setAttribute("id", "chart2");
-	charts.appendChild(chart);
-
-	// set the dimensions and margins of the graph
-	var margin = { top: 30, right: 30, bottom: 130, left: 60 },
-		width = 800 - margin.left - margin.right,
-		height = 600 - margin.top - margin.bottom;
-
-	// append the svg object to the body of the page
-	var svg = d3
-		.select("#chart2")
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// X axis
-	var x = d3
-		.scaleBand()
-		.range([0, width])
-		.domain(
-			userData.xpByProject.map(function (d) {
-				return d.project;
-			})
-		)
-		.padding(0.2);
-	svg
-		.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x))
-		.selectAll("text")
-		.attr("transform", "translate(-10,0)rotate(-45)")
-		.style("text-anchor", "end");
-
-	// Add Y axis
-	var y = d3
-		.scaleLinear()
-		.domain([0, Math.max(...userData.xpByProject.map((o) => o.xp)) + 5000])
-		.range([height, 0]);
-	svg
-		.append("g")
-		.call(d3.axisLeft(y))
-		.append("text")
-		.attr("fill", "#000")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("PROJECT XP");
-
-	// Bars
-	svg
-		.selectAll("mybar")
-		.data(userData.xpByProject)
-		.enter()
-		.append("rect")
-		.attr("x", function (d) {
-			return x(d.project);
-		})
-		.attr("y", function (d) {
-			return y(d.xp);
-		})
-		.attr("width", x.bandwidth())
-		.attr("height", function (d) {
-			return height - y(d.xp);
-		})
-		.attr("fill", "#0b7dda");
+	
 };
   
 
@@ -542,7 +467,3 @@ const xpByProjectChart = (_) => {
               window.location.assign('login.html');
           }
   }
-  
-      //getID();
-    //   getStat();
-      //completedTasks();
